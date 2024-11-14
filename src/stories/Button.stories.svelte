6e7b8f63c715960e02b53a149e73ce2f27796348
Alex <alex.tran1502@gmail.com>
2024-11-14 13:43:31 -0600

    add sample Button component

---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from '../lib/components/Button.svelte';
+	import { fn } from '@storybook/test';
+
+	const { Story } = defineMeta({
+		title: 'Button',
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {},
+		args: {
+			onclick: fn(),
+		},
+	});
+</script>
+
+<Story name="Primary" args={{ color: 'primary' }} />
+<Story name="Secondary" args={{ color: 'secondary' }} />
+<Story name="Success" args={{ color: 'success' }} />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	darkMode: 'class',
+	darkMode: ['class', '[data-mode="dark"]'],
 	theme: {
 		extend: {
 			colors: {
