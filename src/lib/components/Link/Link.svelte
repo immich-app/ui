9ce910497c3c4468b797b17dc90c95111adfb0b4
Jason Rasmussen <jason@rasm.me>
2024-11-18 14:38:39 -0500

    feat: link (#20)

---
+++ src/lib/components/Link/Link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { cleanClass } from '$lib/utils.js';
+	import type { Snippet } from 'svelte';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+	type Props = {
+		class?: string;
+		children: Snippet;
+		href: string;
+	} & HTMLAnchorAttributes;
+
+	const { href, class: className, children, ...restProps }: Props = $props();
+</script>
+
+<a {href} class={cleanClass('underline', className)} {...restProps}>
+	{@render children()}
+</a>
---
+++ src/routes/examples/link/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import DualThemeLayout from '$docs/components/DualThemeLayout.svelte';
+	import Text from '$lib/components/Text/Text.svelte';
+	import { Card, CardBody, CardHeader, CardTitle, Link, VStack } from '@immich/ui';
+</script>
+
+<DualThemeLayout name="Link">
+	{#snippet component()}
+		<Card>
+			<CardHeader>
+				<CardTitle>Basic</CardTitle>
+			</CardHeader>
+			<CardBody>
+				<VStack>
+					<Text><Link href="#">Visit Immich UI</Link></Text>
+					<Text>Read the <Link href="#">release notes</Link></Text>
+					<Text>See the <Link href="#">documentation</Link> for more information</Text>
+				</VStack>
+			</CardBody>
+		</Card>
+	{/snippet}
+</DualThemeLayout>
