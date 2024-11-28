ff27393b8986d9384f77acbbc14f7f1247394da8
Zack Pollard <zackpollard@ymail.com>
2024-11-28 16:07:14 +0000

    ci: fix incorrect module git urls (#33)

--- deployment/modules/cloudflare/pages-project/pages.tf
+++ deployment/modules/cloudflare/pages-project/pages.tf
@@ -1,5 +1,5 @@
 module "pages_project" {
-  source = "git::https://github.com/immich-app/devtools.git//tf/shared/modules/cloudflare-pages-project?ref=feat/pages-project-tf-module"
+  source = "git::https://github.com/immich-app/devtools.git//tf/shared/modules/cloudflare-pages-project?ref=main"
 
   cloudflare_api_token          = data.terraform_remote_state.api_keys_state.outputs.terraform_key_cloudflare_account
   cloudflare_account_id         = data.terraform_remote_state.cloudflare_account.outputs.cloudflare_account_id
--- deployment/modules/cloudflare/static-pages/pages.tf
+++ deployment/modules/cloudflare/static-pages/pages.tf
@@ -1,5 +1,5 @@
 module "static_pages" {
-  source = "git::https://github.com/immich-app/devtools.git//tf/shared/modules/cloudflare-pages?ref=feat/pages-project-tf-module"
+  source = "git::https://github.com/immich-app/devtools.git//tf/shared/modules/cloudflare-pages?ref=main"
 
   cloudflare_api_token          = data.terraform_remote_state.api_keys_state.outputs.terraform_key_cloudflare_docs
   cloudflare_account_id         = data.terraform_remote_state.cloudflare_account.outputs.cloudflare_account_id
