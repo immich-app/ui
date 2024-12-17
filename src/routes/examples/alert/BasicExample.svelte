27f69129ff7839da4a5257aca6f80a2674434d55
Jason Rasmussen <jason@rasm.me>
2024-12-16 22:57:07 -0500

    feat: code toggle for alert examples

---
+++ src/routes/examples/alert/BasicExample.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Alert, Stack } from '@immich/ui';
+</script>
+
+<Stack>
+	<Alert color="primary" title="This is an alert" />
+	<Alert color="secondary" title="This is an alert" />
+	<Alert color="success" title="This is an alert" />
+	<Alert color="info" title="This is an alert" />
+	<Alert color="warning" title="This is an alert" />
+	<Alert color="danger" title="This is an alert" />
+</Stack>
---
+++ src/routes/examples/alert/ColorExample.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Lorem from '$docs/components/Lorem.svelte';
+	import { Alert, Stack } from '@immich/ui';
+</script>
+
+<Stack>
+	<Alert color="primary" title="This is a primary alert">
+		<Lorem />
+	</Alert>
+	<Alert color="secondary" title="This is a secondary alert">
+		<Lorem />
+	</Alert>
+	<Alert color="success" title="This is a success alert">
+		<Lorem />
+	</Alert>
+	<Alert color="info" title="This is an info alert">
+		<Lorem />
+	</Alert>
+	<Alert color="warning" title="This is a warning alert">
+		<Lorem />
+	</Alert>
+	<Alert color="danger" title="This is a danger alert">
+		<Lorem />
+	</Alert>
+</Stack>
