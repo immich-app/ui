4b9e6dd3f58c70074d3eaf9d7e4a280b646825cf
Jason Rasmussen <jason@rasm.me>
2024-12-23 18:10:27 -0500

    feat: code component (#51)

---
+++ src/routes/components/code/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ExampleLayout from '$docs/components/ExampleLayout.svelte';
+	import ColorExample from './ColorExample.svelte';
+	import colorExample from './ColorExample.svelte?raw';
+	import FilledExample from './FilledExample.svelte';
+	import filledExample from './FilledExample.svelte?raw';
+	import SizeExample from './SizeExample.svelte';
+	import sizeExample from './SizeExample.svelte?raw';
+
+	const examples = [
+		{ title: 'Size', code: sizeExample, component: SizeExample },
+		{ title: 'Color', code: colorExample, component: ColorExample },
+		{ title: 'Filled', code: filledExample, component: FilledExample },
+	];
+</script>
+
+<ExampleLayout name="Code" {examples} />
