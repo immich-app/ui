59e3eeae327d03c56d2fb55154ae887fed05186e
Alex <alex.tran1502@gmail.com>
2025-01-21 12:18:01 -0600

    chore: better size indication for button component (#87)
    * chore: better size indication for button component
    
    * use tailwindcss

--- src/routes/components/button/SizeExample.svelte
+++ src/routes/components/button/SizeExample.svelte
@@ -1,60 +1,45 @@
 <script lang="ts">
-	import { Button, Heading, HStack, Stack } from '@immich/ui';
+	import { Button, Code, Heading } from '@immich/ui';
 </script>
 
-<HStack gap={4}>
-	<Stack>
+<section class="grid grid-cols-[70px_1fr_1fr_1fr] gap-4">
+	<!-- Size Column -->
+	<div class="grid grid-cols-1 grid-rows-6 place-items-center justify-items-start gap-2">
+		<Heading size="tiny">Size</Heading>
+		<Code size="tiny">Tiny</Code>
+		<Code size="tiny">Small</Code>
+		<Code size="tiny">Medium</Code>
+		<Code size="tiny">Large</Code>
+		<Code size="tiny">Giant</Code>
+	</div>
+
+	<!-- Round Column -->
+	<div class="grid grid-cols-1 grid-rows-6 place-items-center justify-items-start gap-2">
 		<Heading size="tiny">Round</Heading>
-		<div>
-			<Button shape="round" size="tiny">Tiny</Button>
-		</div>
-		<div>
-			<Button shape="round" size="small">Small</Button>
-		</div>
-		<div>
-			<Button shape="round" size="medium">Medium</Button>
-		</div>
-		<div>
-			<Button shape="round" size="large">Large</Button>
-		</div>
-		<div>
-			<Button shape="round" size="giant">Giant</Button>
-		</div>
-	</Stack>
-	<Stack>
+		<Button shape="round" size="tiny">Click me</Button>
+		<Button shape="round" size="small">Click me</Button>
+		<Button shape="round" size="medium">Click me</Button>
+		<Button shape="round" size="large">Click me</Button>
+		<Button shape="round" size="giant">Click me</Button>
+	</div>
+
+	<!-- Semi-Round Column -->
+	<div class="grid grid-cols-1 grid-rows-6 place-items-center justify-items-start gap-2">
 		<Heading size="tiny">Semi-Rounded</Heading>
-		<div>
-			<Button size="tiny">Tiny</Button>
-		</div>
-		<div>
-			<Button size="small">Small</Button>
-		</div>
-		<div>
-			<Button size="medium">Medium</Button>
-		</div>
-		<div>
-			<Button size="large">Large</Button>
-		</div>
-		<div>
-			<Button size="giant">Giant</Button>
-		</div>
-	</Stack>
-	<Stack>
+		<Button size="tiny">Click me</Button>
+		<Button size="small">Click me</Button>
+		<Button size="medium">Click me</Button>
+		<Button size="large">Click me</Button>
+		<Button size="giant">Click me</Button>
+	</div>
+
+	<!-- Rectangle Column -->
+	<div class="grid grid-cols-1 grid-rows-6 place-items-center justify-items-start gap-2">
 		<Heading size="tiny">Rectangle</Heading>
-		<div>
-			<Button shape="rectangle" size="tiny">Tiny</Button>
-		</div>
-		<div>
-			<Button shape="rectangle" size="small">Small</Button>
-		</div>
-		<div>
-			<Button shape="rectangle" size="medium">Medium</Button>
-		</div>
-		<div>
-			<Button shape="rectangle" size="large">Large</Button>
-		</div>
-		<div>
-			<Button shape="rectangle" size="giant">Giant</Button>
-		</div>
-	</Stack>
-</HStack>
+		<Button shape="rectangle" size="tiny">Click me</Button>
+		<Button shape="rectangle" size="small">Click me</Button>
+		<Button shape="rectangle" size="medium">Click me</Button>
+		<Button shape="rectangle" size="large">Click me</Button>
+		<Button shape="rectangle" size="giant">Click me</Button>
+	</div>
+</section>
