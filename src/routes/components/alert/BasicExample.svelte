7b757f78723d8c8baf8d1eabffc6a65ff11f48c8
Jason Rasmussen <jason@rasm.me>
2025-01-24 13:37:03 -0500

    feat: alert improvements

--- src/routes/components/alert/BasicExample.svelte
+++ src/routes/components/alert/BasicExample.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+	import Lorem from '$docs/components/Lorem.svelte';
 	import { Alert, Stack } from '@immich/ui';
 </script>
 
 <Stack>
-	<Alert color="primary" title="This is an alert" />
-	<Alert color="secondary" title="This is an alert" size="tiny" />
-	<Alert color="success" title="This is an alert" size="small" />
-	<Alert color="info" title="This is an alert" size="medium" />
-	<Alert color="warning" title="This is an alert" size="large" />
-	<Alert color="danger" title="This is an alert" size="giant" />
+	<Alert title="This is an alert" />
+	<Alert>
+		<p>This is an alert</p>
+	</Alert>
+	<Alert color="primary" title="This is a primary alert">
+		<Lorem />
+	</Alert>
 </Stack>
