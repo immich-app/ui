efed1e305942ea8ac5619a9b168863bdb847cb96
Jason Rasmussen <jason@rasm.me>
2025-05-02 12:53:53 -0400

    feat: modal icon

--- src/routes/components/modal/+page.svelte
+++ src/routes/components/modal/+page.svelte
@@ -2,13 +2,15 @@
 	import ComponentExamples from '$docs/components/ComponentExamples.svelte';
 	import ComponentNoteCard from '$docs/components/ComponentNoteCard.svelte';
 	import ComponentPage from '$docs/components/ComponentPage.svelte';
+	import { Text } from '@immich/ui';
 	import BasicExample from './BasicExample.svelte';
 	import basicExample from './BasicExample.svelte?raw';
+	import IconExample from './IconExample.svelte';
+	import iconExample from './IconExample.svelte?raw';
 	import RouteExample from './RouteExample.svelte';
 	import routeExample from './RouteExample.svelte?raw';
 	import SizeExample from './SizeExample.svelte';
 	import sizeExample from './SizeExample.svelte?raw';
-	import { Text } from '@immich/ui';
 </script>
 
 <ComponentPage name="Modal">
@@ -18,6 +20,7 @@
 	<ComponentExamples
 		examples={[
 			{ title: 'Basic', code: basicExample, component: BasicExample },
+			{ title: 'Icon', code: iconExample, component: IconExample },
 			{ title: 'Size', code: sizeExample, component: SizeExample },
 			{ title: 'Route', code: routeExample, component: RouteExample },
 		]}
