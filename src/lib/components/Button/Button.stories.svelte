638397b535b5c6899d304667807f3a98c459277b
ben-basten <45583362+ben-basten@users.noreply.github.com>
2025-05-07 10:06:30 -0400

    wip: initial commit

---
+++ src/lib/components/Button/Button.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	import Button from './Button.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Button',
+		component: Button,
+		decorators: [
+			/* ... */
+		],
+		parameters: {
+			/* ... */
+		},
+	});
+</script>
+
+<Story name="Default">Test</Story>
