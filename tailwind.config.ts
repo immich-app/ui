80de1a2c7687a4ca1c9db95223e0ac631f81a4fd
Jason Rasmussen <jason@rasm.me>
2025-05-15 10:40:00 -0400

    feat: default theme (#170)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,15 @@
 import type { Config } from 'tailwindcss';
+import { tailwindConfig } from './src/lib/theme/default.js';
+
+const { colors, borderColor } = tailwindConfig();
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	darkMode: 'class',
 	theme: {
 		extend: {
-			colors: {
-				primary: 'rgb(var(--immich-primary) / <alpha-value>)',
-				light: 'rgb(var(--immich-light) / <alpha-value>)',
-				dark: 'rgb(var(--immich-dark) / <alpha-value>)',
-				success: 'rgb(var(--immich-success) / <alpha-value>)',
-				danger: 'rgb(var(--immich-danger) / <alpha-value>)',
-				warning: 'rgb(var(--immich-warning) / <alpha-value>)',
-				info: 'rgb(var(--immich-info) / <alpha-value>)',
-				subtle: 'rgb(var(--immich-gray) / <alpha-value>)',
-			},
-			borderColor: ({ theme }) => ({
-				...theme('colors'),
-				DEFAULT: 'rgb(var(--immich-default-border) / <alpha-value>)',
-			}),
+			colors,
+			borderColor,
 		},
 	},
 
