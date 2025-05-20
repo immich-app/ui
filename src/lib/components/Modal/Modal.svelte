73addcef491cf8deb14e4d4eddb3497ead1d24c1
Arno Wiest <arnolicious.git@gmail.com>
2025-05-20 23:47:03 +0200

    temp: event stuff

--- src/lib/components/Modal/Modal.svelte
+++ src/lib/components/Modal/Modal.svelte
@@ -61,7 +61,7 @@
 	};
 </script>
 
-<svelte:document
+<!-- <svelte:document
 	onkeydown={(e) => {
 		if (e.key === 'Escape' && open) {
 			// Stop propagation to ensure modals close before immich-web takes over
@@ -70,11 +70,19 @@
 			onClose?.();
 		}
 	}}
-/>
+/> -->
 <Dialog.Root {open} onOpenChange={onChange}>
 	<Dialog.Portal>
 		<Dialog.Overlay class="absolute start-0 top-0 flex h-dvh w-screen bg-black/30" />
 		<Dialog.Content
+			onkeydown={(e) => {
+				if (e.key === 'Escape' && open) {
+					// Stop propagation to ensure modals close before immich-web takes over
+					e.stopPropagation();
+					open = false;
+					onClose?.();
+				}
+			}}
 			class={cleanClass(
 				'absolute start-0 top-0 flex h-dvh w-screen items-center justify-center overflow-hidden sm:p-4',
 			)}
