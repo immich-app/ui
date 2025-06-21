a3fe45933e3758c9cb2321400ecced8758ad7bc5
Daniel Dietzler <mail@ddietzler.dev>
2025-06-21 22:33:35 +0200

    feat: modal manager

---
+++ src/routes/components/modal/BasicModal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Lorem from '$docs/components/Lorem.svelte';
+	import { Modal, ModalBody } from '@immich/ui';
+
+	type Props = { onClose: () => void };
+
+	const { onClose }: Props = $props();
+</script>
+
+<Modal title="Modal Title" {onClose}>
+	<ModalBody>
+		<Lorem />
+	</ModalBody>
+</Modal>
