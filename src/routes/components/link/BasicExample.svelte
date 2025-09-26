c9e212960ad5e3b84b9e04ca37b9497559089ac0
Jason Rasmussen <jason@rasm.me>
2025-09-26 10:56:04 -0400

    feat: link shorthand

--- src/routes/components/link/BasicExample.svelte
+++ src/routes/components/link/BasicExample.svelte
@@ -5,6 +5,12 @@
 <Text><Link href="#">Visit Immich UI</Link></Text>
 <Text>Read the <Link href="#">release notes</Link></Text>
 <Text>See the <Link href="#">documentation</Link> for more information</Text>
+
 <br />
-<Text><Link href="/">Internal link</Link></Text>
-<Text><Link href="https://immich.app">External link</Link></Text>
+
+<Text>Examples:</Text>
+<ul class="list-disc ps-6">
+  <li><b>Internal &mdash;</b> <Link href="/">Internal link</Link></li>
+  <li><b>External &mdash;</b> <Link href="https://immich.app">External link</Link></li>
+  <li><b>Shorthand &mdash;</b> <Link href="https://immich.app" /></li>
+</ul>
