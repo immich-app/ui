8d1550bdcac44d5b3cc2e1c61791d48e4cb57985
Alex Tran <Alex.Tran@conductix.com>
2025-10-10 12:09:43 -0500

    feat: new footer

--- src/lib/site/SiteFooterLink.svelte
+++ src/lib/site/SiteFooterLink.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
   import HStack from '$lib/components/Stack/HStack.svelte';
-  import Icon from '$lib/components/Icon/Icon.svelte';
   import Link from '$lib/components/Link/Link.svelte';
   import Text from '$lib/components/Text/Text.svelte';
 
   type Props = {
     href: string;
-    icon: string | { path: string };
     text: string;
   };
 
-  let { href, icon, text }: Props = $props();
+  let { href, text }: Props = $props();
 </script>
 
-<Link {href}>
+<Link {href} underline={false} class="text-dark/80 hover:text-primary transition-colors">
   <HStack>
-    <Icon {icon} size="1.5em" />
     <Text>{text}</Text>
   </HStack>
 </Link>
