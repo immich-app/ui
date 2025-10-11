234c7401858534bd86e2ecf9d36b10a467a0242b
Alex <alex.tran1502@gmail.com>
2025-10-11 16:51:16 -0500

    chore: footer style tweak (#304)

--- src/lib/site/SiteFooter.svelte
+++ src/lib/site/SiteFooter.svelte
@@ -3,7 +3,6 @@
   import Stack from '$lib/components/Stack/Stack.svelte';
   import VStack from '$lib/components/Stack/VStack.svelte';
   import Text from '$lib/components/Text/Text.svelte';
-  import ThemeSwitcher from '$lib/components/ThemeSwitcher/ThemeSwitcher.svelte';
   import { Constants } from '$lib/site/constants.js';
   import SiteFooterLink from '$lib/site/SiteFooterLink.svelte';
   import {
@@ -21,49 +20,51 @@
   import { siAndroid, siApple, siDiscord, siGithub, siReddit } from 'simple-icons';
 </script>
 
-<div class="mt-16 rounded-t-3xl bg-gray-100 p-8 dark:bg-neutral-900">
+<div class="mt-16 rounded-t-md bg-gray-50 p-8 lg:p-0 dark:bg-neutral-900">
   <div class="mx-auto max-w-(--breakpoint-lg) xl:py-8">
     <Stack gap={8}>
       <div class="place-center grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
         <Stack>
-          <Heading size="tiny">Social</Heading>
+          <Heading size="small" class="mb-3">Social</Heading>
           <SiteFooterLink href={Constants.Socials.Github} icon={siGithub} text="GitHub" />
           <SiteFooterLink href={Constants.Socials.Discord} icon={siDiscord} text="Discord" />
           <SiteFooterLink href={Constants.Socials.Reddit} icon={siReddit} text="Reddit" />
         </Stack>
 
         <Stack>
-          <Heading size="tiny">Download</Heading>
+          <Heading size="small" class="mb-3">Download</Heading>
           <SiteFooterLink href={Constants.Get.Android} icon={siAndroid} text="Android" />
           <SiteFooterLink href={Constants.Get.iOS} icon={siApple} text="iOS" />
           <SiteFooterLink href={Constants.Get.Download} icon={mdiServerOutline} text="Server" />
         </Stack>
 
         <Stack>
-          <Heading size="tiny">Company</Heading>
+          <Heading size="small" class="mb-3">Company</Heading>
           <SiteFooterLink href={Constants.Socials.Futo} icon={mdiOfficeBuildingOutline} text="FUTO" />
           <SiteFooterLink href={Constants.Sites.Buy} icon={mdiKey} text="Purchase" />
           <SiteFooterLink href={Constants.Sites.Store} icon={mdiShoppingOutline} text="Merch" />
         </Stack>
 
         <Stack>
-          <Heading size="tiny">Sites</Heading>
+          <Heading size="small" class="mb-3">Sites</Heading>
           <SiteFooterLink href={Constants.Sites.Docs} icon={mdiScriptTextOutline} text="Documentation" />
           <SiteFooterLink href={Constants.Sites.My} icon={mdiHomeSearchOutline} text="My Immich" />
           <SiteFooterLink href={Constants.Sites.Api} icon={mdiCubeOutline} text="Immich API" />
         </Stack>
 
         <Stack>
-          <Heading size="tiny">Miscellaneous</Heading>
+          <Heading size="small" class="mb-3">Miscellaneous</Heading>
           <SiteFooterLink href={Constants.Pages.Roadmap} icon={mdiChartGantt} text="Roadmap" />
           <SiteFooterLink href={Constants.Pages.CursedKnowledge} icon={mdiBookshelf} text="Cursed Knowledge" />
           <SiteFooterLink href={Constants.Pages.PrivacyPolicy} icon={mdiShieldAccountOutline} text="Privacy Policy" />
         </Stack>
       </div>
+
+      <hr />
+
       <VStack class="text-center">
         <Text size="large">This project is available under GNU AGPL v3 license.</Text>
         <Text color="muted" variant="italic">Privacy should not be a luxury</Text>
-        <ThemeSwitcher color="secondary" />
       </VStack>
     </Stack>
   </div>
