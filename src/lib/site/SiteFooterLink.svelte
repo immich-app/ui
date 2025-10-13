54990511fd02a73bae8fe76f92ff4585a862855f
Jason Rasmussen <jason@rasm.me>
2025-10-13 17:25:53 -0400

    fix: simpler footer (#309)
    chore: simpler site-footer

--- src/lib/site/SiteFooterLink.svelte
+++ src/lib/site/SiteFooterLink.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
-  import HStack from '$lib/components/Stack/HStack.svelte';
-  import Icon from '$lib/components/Icon/Icon.svelte';
   import Link from '$lib/components/Link/Link.svelte';
   import Text from '$lib/components/Text/Text.svelte';
 
   type Props = {
     href: string;
-    icon: string | { path: string };
     text: string;
   };
 
-  let { href, icon, text }: Props = $props();
+  let { href, text }: Props = $props();
 </script>
 
 <Link {href} underline={false} class="text-dark/90 hover:text-primary transition-colors">
-  <HStack>
-    <Icon {icon} size="1.2em" />
-    <Text>{text}</Text>
-  </HStack>
+  <Text>{text}</Text>
 </Link>
