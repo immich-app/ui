657cfa311d9b344af94f11a130e6364d983d8ee8
Jason Rasmussen <jason@rasm.me>
2025-10-30 17:33:20 -0400

    feat: default layout (#365)

--- packages/svelte-markdown-preprocess/src/index.ts
+++ packages/svelte-markdown-preprocess/src/index.ts
@@ -14,26 +14,28 @@ type Options = {
 };
 
 const resolveLayout = (layout?: string, layouts?: Layouts) => {
-  if (layout && layouts) {
-    return layouts[layout];
+  if (!layouts) {
+    return;
   }
+
+  return layouts[layout || 'default'];
 };
 
 const SCRIPT_BODY_REGEX = /<script.*>(?<body>(.|\n)*?)<\/script>/;
 
 const parse = (content: string) => {
-  let scriptBody = '';
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const { attributes, body: bodyParsed } = fm(content);
 
-  const match = SCRIPT_BODY_REGEX.exec(content);
+  let scriptBody = '';
+  let body = bodyParsed;
+  const match = SCRIPT_BODY_REGEX.exec(body);
   const bodyGroup = match?.groups?.body;
   if (bodyGroup) {
     scriptBody = bodyGroup.trim();
-    content = content.substring(match.index + match[0].length).trim();
+    body = body.substring(match.index + match[0].length).trim();
   }
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const { attributes, body } = fm(content);
 
   return { attributes, body, scriptBody };
 };
@@ -49,6 +51,7 @@ export const svelteMarkdownPreprocess = (options: Options) => {
       }
 
       const { attributes, body, scriptBody } = parse(content);
+      const markup = await md.parse(body);
 
       const file = new SvelteFile();
       file.addScript(`import { Markdown } from '${markdownPackageName}';`);
@@ -56,11 +59,11 @@ export const svelteMarkdownPreprocess = (options: Options) => {
       const layout = resolveLayout(attributes.layout, layouts);
       if (layout) {
         file.addScript(`import Layout from '${layout}';`);
-        file.addTag(`<Layout>`, '</Layout>');
+        file.addTag(`<Layout attributes={${JSON.stringify(attributes)}}>`, '</Layout>');
       }
 
       file.addScript(scriptBody);
-      file.addTemplate(await md.parse(body));
+      file.addTemplate(markup);
 
       const code = file.export();
 
