9c47c59906d1f5f6261e19dd3b3e55b8c3d6bfac
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2025-11-05 20:43:54 +0100

    feat: command palette shortcuts (#379)
    * feat: command palette shortcuts
    
    * fix: bug
    
    ---------
    
    Co-authored-by: Jason Rasmussen <jason@rasm.me>

--- packages/ui/src/lib/components/CommandPalette/CommandPaletteContext.svelte
+++ packages/ui/src/lib/components/CommandPalette/CommandPaletteContext.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { shortcuts } from '$lib/actions/shortcut.js';
   import { commandPaletteManager, type CommandItem } from '$lib/services/command-palette-manager.svelte';
   import { onMount } from 'svelte';
 
@@ -10,15 +9,5 @@
 
   const { commands = [], global }: Props = $props();
 
-  const handleOpen = () => commandPaletteManager.open();
-
   onMount(() => commandPaletteManager.addCommands(commands, { global }));
 </script>
-
-<svelte:window
-  use:shortcuts={[
-    { shortcut: { key: 'k', meta: true }, onShortcut: handleOpen },
-    { shortcut: { key: 'k', ctrl: true }, onShortcut: handleOpen },
-    { shortcut: { key: '/' }, preventDefault: true, onShortcut: handleOpen },
-  ]}
-/>
