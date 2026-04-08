f31ba2c7c7a44b4447c68531ac78adab8e523c56
Jason Rasmussen <jason@rasm.me>
2026-04-08 12:45:25 -0400

    feat: github link

---
+++ packages/ui/src/routes/components/github-link/BasicExample.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { GithubLink, Heading } from '@immich/ui';
+</script>
+
+<Heading size="small">Default</Heading>
+<GithubLink number={1000} />
+
+<Heading size="small" class="mt-4">Another repo</Heading>
+<GithubLink repo="static-pages" number={64} />
+
+<Heading size="small" class="mt-4">Another org</Heading>
+<GithubLink org="kysely-org" repo="kysely" number={64} />
+
+<Heading size="small" class="mt-4">No icon</Heading>
+<GithubLink number={64} icon={false} />
