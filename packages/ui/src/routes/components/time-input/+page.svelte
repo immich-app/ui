9c96c256ef41698e9069fe9016a49bf5647c90e8
Jason Rasmussen <jason@rasm.me>
2026-04-07 21:20:15 -0400

    feat: time input (#645)

--- packages/ui/src/routes/components/time-input/+page.svelte
+++ packages/ui/src/routes/components/time-input/+page.svelte
@@ -8,7 +8,7 @@
   import basicExample from './BasicExample.svelte?raw';
 </script>
 
-<ComponentPage name="NumberInput" description="Accept number input from the user">
+<ComponentPage name="TimeInput" description="Accept number input from the user">
   <ComponentTipCard>
     <Text>
       See <ComponentLink name="Input" /> for more information about states, shapes, sizes, and more.
